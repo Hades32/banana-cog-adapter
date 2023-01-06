ecc7389146a500c1ba7b8b001fc509c9cb778cc7
Martin Rauscher <martin@ultimatetournament.io>
2023-01-06 19:32:15 +0100

    handle interrupt signal

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -55,6 +57,14 @@ func main() {
 	})
 	go s.ListenAndServe()
 	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		fmt.Println("got interrupt signal")
+		s.Shutdown(context.Background())
+		os.Exit(0)
+	}()
 	cmd.Run()
 	fmt.Println("command finished - shutting down adapter")
 	s.Shutdown(context.Background())
